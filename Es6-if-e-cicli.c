797dbbbb12db36d5eb4523e16a5664d35f9a86bf
viseeeee <luca.visentin.1e@iisviolamarchesini.edu.it>
2024-09-18 10:16:01 -0400

    esercizi if e cicli

---
+++ Es6-if-e-cicli.c
@@ -0,0 +1,24 @@
+/*Progettare un algoritmo che effettui le seguenti operazioni:
+• legga da tastiera una coppia di valori interi A e B con A<B;
+• continui a leggere da tastiera una serie di valori interi, terminando
+quando il valore letto è al di fuori dell’intervallo [A, B];
+• conteggi la media dei valori letti;
+• prima di terminare, stampi il valore calcolato.*/
+#include <stdio.h>
+void main()
+{
+    int a, b, somma = 0, valoriInseriti = 0;
+    double media;
+    do
+    {
+        printf("Inserisci il valore del primo numero\n");
+        scanf("%d", &a);
+
+        printf("Inserisci il valore del secondo numero\n");
+        scanf("%d", &b);
+        valoriInseriti=valoriInseriti+2;
+        somma = somma + a + b;
+    } while (a > b);
+    media = (double)somma / (double)valoriInseriti;
+    printf("La media di tutti i numeri inseriti è:%.2f\n",media);
+}
